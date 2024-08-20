28d10b53207ed40e9835ac6509ba805528ddd294
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-21 00:42:44 +0300

    KS-2-2 spaces added

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -5,9 +5,9 @@ fun main() {
     val salaryPermanentEmployees = 30000
     val countOfInterns = 30
     val salaryInterns = 20000
-    val summarySalaryPermanent = countOfPermanentEmployees*salaryPermanentEmployees
-    val salaryAllEmployees = summarySalaryPermanent+countOfInterns*salaryInterns
-    val averageSalary = salaryAllEmployees/(countOfInterns+countOfPermanentEmployees)
+    val summarySalaryPermanent = countOfPermanentEmployees * salaryPermanentEmployees
+    val salaryAllEmployees = summarySalaryPermanent+countOfInterns * salaryInterns
+    val averageSalary = salaryAllEmployees / (countOfInterns + countOfPermanentEmployees)
     println(summarySalaryPermanent)
     println(salaryAllEmployees)
     println(averageSalary)
