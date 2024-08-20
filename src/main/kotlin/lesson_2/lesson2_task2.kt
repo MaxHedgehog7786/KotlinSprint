1550c5d9ee5433c73912ba7581b7f8b9de001636
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-20 22:17:31 +0300

    KS-2-2 simulation of accounting

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    val salaryPermanentEmployees = 50*30000
+    val salaryAllEmployees = salaryPermanentEmployees+30*20000
+    val averageSalary = salaryAllEmployees/80
+    println(salaryPermanentEmployees)
+    println(salaryAllEmployees)
+    println(averageSalary)
+}
