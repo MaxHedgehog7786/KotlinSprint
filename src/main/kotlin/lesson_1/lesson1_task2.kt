87b8923df066893da90d21e5976e14dfc69297c8
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-20 15:29:54 +0300

    KS-1-2 create variable for employees

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val countOfOrders: Int = 75
+    val thanksWords: String = "Спасибо за заказ!"
+    println(countOfOrders)
+    println(thanksWords)
+    var countOfEmployees: Int = 2000
+    //println(countOfEmployees)
+    countOfEmployees = 1999
+    println(countOfEmployees)
+}
