22c026a2358fbd6f4987c444304434d772ded541
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-21 00:42:10 +0300

    KS-2-2 magic numbers

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,10 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
-    val salaryPermanentEmployees = 50*30000
-    val salaryAllEmployees = salaryPermanentEmployees+30*20000
-    val averageSalary = salaryAllEmployees/80
-    println(salaryPermanentEmployees)
+    val countOfPermanentEmployees = 50
+    val salaryPermanentEmployees = 30000
+    val countOfInterns = 30
+    val salaryInterns = 20000
+    val summarySalaryPermanent = countOfPermanentEmployees*salaryPermanentEmployees
+    val salaryAllEmployees = summarySalaryPermanent+countOfInterns*salaryInterns
+    val averageSalary = salaryAllEmployees/(countOfInterns+countOfPermanentEmployees)
+    println(summarySalaryPermanent)
     println(salaryAllEmployees)
     println(averageSalary)
 }
