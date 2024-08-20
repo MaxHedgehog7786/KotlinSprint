95fa6179498aa58a32ad1f2087ebcb24f413243a
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-21 00:35:19 +0300

    KS-1-5 spaces added

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -3,10 +3,10 @@ package org.example.lesson_1
 fun main() {
     val seconds: Short = 6480
     val secondsPerMinute: Byte = 60
-    val minutes: Int = seconds/secondsPerMinute
-    val secondsRemains: Int = seconds%secondsPerMinute
-    val hours: Int = minutes/secondsPerMinute
-    val minutesRemains: Int = minutes%secondsPerMinute
+    val minutes: Int = seconds / secondsPerMinute
+    val secondsRemains: Int = seconds % secondsPerMinute
+    val hours: Int = minutes / secondsPerMinute
+    val minutesRemains: Int = minutes % secondsPerMinute
     print(String.format("%02d:%02d:%02d", hours,minutesRemains, secondsRemains))
 
 }
