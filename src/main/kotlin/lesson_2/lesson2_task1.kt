4c18339a12224bb552454b81873167705d6ad9ab
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-21 17:18:16 +0300

    KS-2-1 removed magic numbers

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,5 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
-    println(String.format("%.02f", (3 + 4 + 3 + 5) / 4.0))
+    val countOfStudents: Int = 4
+    val firstScore = 3
+    val secondScore = 4
+    val thirdScore = 3
+    val fourthScore = 5
+
+    val sumOfScores = (firstScore + secondScore + thirdScore + fourthScore).toDouble()
+    val averageScore = sumOfScores / countOfStudents
+
+    println(String.format("%.02f", averageScore))
 }
