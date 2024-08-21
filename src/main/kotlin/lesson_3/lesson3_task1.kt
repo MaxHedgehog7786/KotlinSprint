fb678b8b85222c7369bc54b88ecee030cd4a0b95
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-21 17:37:46 +0300

    KS-3-1 created variables to greet the user

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    var greeting = "Добрый день"
+    val userName = "Максим"
+
+    println("$greeting, $userName!")
+
+    greeting = "Добрый вечер"
+
+    println("$greeting, $userName!")
+}
