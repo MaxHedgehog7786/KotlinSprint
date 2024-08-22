e556c6183618ab75b5b04adcaa0722c71b16ab02
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-23 01:41:12 +0300

    KS-4-2 formated strings

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -4,12 +4,14 @@ fun main() {
     var cargoWeight = 20
     var cargoVolume = 80
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME}")
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
+            "${cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME}")
 
     cargoWeight = 50
     cargoVolume = 100
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME}")
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
+            "${cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME}")
 
 }
 
