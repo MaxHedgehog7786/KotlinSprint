63450478e8669632b5a9892dad67022fd1bb0a4a
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-23 01:46:24 +0300

    KS-4-4 compliance with the principle DRY

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,10 +2,12 @@ package org.example.lesson_4
 
 fun main() {
     val trainingDay = 5
+    val isOddDay = trainingDay % 2 == 1
+    val isEvenDay = !isOddDay
 
-    println("""Упражнения для рук: ${trainingDay % 2 == 1}
-        |Упражнения для ног: ${trainingDay % 2 == 0}
-        |Упражнения для спины ${trainingDay % 2 == 0}
-        |Упражнения для пресса: ${trainingDay % 2 == 1}
+    println("""Упражнения для рук: $isOddDay
+        |Упражнения для ног: $isEvenDay
+        |Упражнения для спины: $isEvenDay
+        |Упражнения для пресса: $isOddDay
     """.trimMargin())
 }
