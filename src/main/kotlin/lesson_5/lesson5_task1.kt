b795ad47b5533755e90188b8013a2387036e2adf
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-24 13:35:08 +0300

    KS-5-3 add necessary files

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+    val maxNumber = 10
+    val firstNumber = Random.nextInt(maxNumber)
+    val secondNumber = Random.nextInt(maxNumber)
+    val captcha = "Чтобы доказать, что вы не бот, сложите два числа: $firstNumber и $secondNumber\n"
+
+    print(captcha)
+    val userAnswer = readln().toInt()
+
+    if (userAnswer == firstNumber + secondNumber){
+        println("Добро пожаловать!")
+    } else{
+        println("Вход запрещен.")
+    }
+}
