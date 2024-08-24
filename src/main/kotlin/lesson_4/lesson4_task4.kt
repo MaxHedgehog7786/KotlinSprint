d09876809cc3761b36a42ba6bf5ca21349288081
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-08-24 13:37:56 +0300

    KS-4-4 memory optimization

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,11 +3,12 @@ package org.example.lesson_4
 fun main() {
     val trainingDay = 5
     val isOddDay = trainingDay % 2 == 1
-    val isEvenDay = !isOddDay
 
-    println("""Упражнения для рук: $isOddDay
-        |Упражнения для ног: $isEvenDay
-        |Упражнения для спины: $isEvenDay
+    println(
+        """Упражнения для рук: $isOddDay
+        |Упражнения для ног: ${!isOddDay}
+        |Упражнения для спины: ${!isOddDay}
         |Упражнения для пресса: $isOddDay
-    """.trimMargin())
+    """.trimMargin()
+    )
 }
